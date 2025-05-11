99aba12745f2bd3609a5d67f1293eee80374c8a6
arplant <alastairrobertplant@gmail.com>
2025-05-11 14:44:04 +0100

    Added basic customtkinter App class.

--- vol_conc_calc/main.py
+++ vol_conc_calc/main.py
@@ -1,1 +1,34 @@
-print("Hello World!")
+
+import customtkinter
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Volume-Concentration Calculator")
+        self.geometry("400x150")
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.checkbox_frame = customtkinter.CTkFrame(self)
+        self.checkbox_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsw")
+
+        self.check_var = customtkinter.StringVar(value="on")
+        self.checkbox_1 = customtkinter.CTkCheckBox(self.checkbox_frame,
+                                                    text="checkbox 1",
+                                                    command=self.checkbox_event,
+                                                    variable=self.check_var,
+                                                    onvalue="on",
+                                                    offvalue="off")
+        self.checkbox_1.grid(row=0, column=0, padx=20, pady=(0, 20), sticky="w")
+
+    def button_callback(self):
+        print("button pressed")
+
+    def checkbox_event(self):
+        print("checkbox toggled, current value:", self.check_var.get())
+
+
+app = App()
+app.mainloop()
+
